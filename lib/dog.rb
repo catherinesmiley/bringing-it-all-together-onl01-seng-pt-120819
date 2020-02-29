ae69848e85ddba657c65b66840c24f950963db8d
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-29 20:54:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,4 +86,14 @@ class Dog
     dog 
   end 
   
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+    
+    DB[:conn].execute(sql, name: name)
+  end 
+  
 end 
