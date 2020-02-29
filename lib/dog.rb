8a65f6ac944d7f8cd0cef6d504e500c26811dd99
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-29 20:51:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,8 +71,12 @@ class Dog
       SELECT * 
       FROM dogs
       WHERE name = ?
+      AND breed = ?
+      LIMIT 1 
     SQL
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", name, breed)
+    
+    dog = DB[:conn].execute(sql, name, breed)
+    
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
