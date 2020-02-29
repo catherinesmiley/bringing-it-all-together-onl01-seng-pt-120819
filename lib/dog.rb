0bdfac907a702ee78d3782799abe2a9aedd458d2
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-29 20:48:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,8 @@ class Dog
   
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", name, breed)
-    if dog 
+    if !dog.empty?
+      
   end 
   
 end 
